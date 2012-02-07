9d972ab95df619a3df5a52837f42e3286a336bca
buzz5 <buzz5@BUZZ5-laptop>
2012-02-06 20:54:34 -0500

    FINALLY FIXED AUTO BALL LOAD!

--- src/org/buzzrobotics/commands/AutoBallLoad.java
+++ src/org/buzzrobotics/commands/AutoBallLoad.java
@@ -32,7 +32,7 @@ public class AutoBallLoad extends CommandBase {
     // Called once after isFinished returns true
     protected void end() {
         rollerarm.turnOffRollers();
-        ballfeeder.driveDown();
+        ballfeeder.stop();
     }
 
     // Called when another command which requires one or more of the same
